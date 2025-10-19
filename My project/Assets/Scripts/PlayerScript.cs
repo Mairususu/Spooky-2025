436965ae3eefad1fba53bb03141701da107dbc87
Hoji24 <theret19@gmail.com>
2025-10-19 18:07:10 +0200

    la fin de la session

--- My project/Assets/Scripts/PlayerScript.cs
+++ My project/Assets/Scripts/PlayerScript.cs
@@ -8,7 +8,9 @@ public class PlayerScript : MonoBehaviour
 {
 
     private Rigidbody2D _rigidbody;
-    [SerializeField] private float speed = 5.0f;
+    [SerializeField] private float speed = 4.0f;
+    [SerializeField] private float spiderLegSpeed = 6.0f;
+    
     [SerializeField] private float dashDistance = 2.0f;
     [SerializeField] private float dashCooldown = 0.5f;
     private float cooldownStart = 0.0f;
@@ -97,6 +99,21 @@ public class PlayerScript : MonoBehaviour
         }
 
         if (collision.gameObject.CompareTag("SpiderCorpse"))
+        {
+            speed = spiderLegSpeed;
+        }
+        
+        if (collision.gameObject.CompareTag("EyesCorpse"))
+        {
+            
+        }
+       
+        if (collision.gameObject.CompareTag("ChtulhuCorpse"))
+        {
+            
+        }
+       
+        if (collision.gameObject.CompareTag("SlendermanCorpse"))
         {
             
         }
