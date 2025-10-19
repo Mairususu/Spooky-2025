2ab653f9b74189851510c7119bf4d20614accd57
LOUVEL OCTAVE p2303652 <octave.louvel@etu.univ-lyon1.fr>
2025-10-19 16:49:10 +0200

    Ajout de l'animation

--- My project/Assets/Visuel/Player/face/SpriteMerge.cs
+++ My project/Assets/Visuel/Player/face/SpriteMerge.cs
@@ -1,17 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class SpriteMerge : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private Sprite[] face = null;
+    [SerializeField] private Sprite finalSprite = null;
 
-    // Update is called once per frame
-    void Update()
+    private void Merge()
     {
         
     }
